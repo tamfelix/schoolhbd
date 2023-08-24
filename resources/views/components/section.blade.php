2ab23bdd36024bd60f4c28eb9efe8da251a466ca
tamfelix <84901549+tamfelix@users.noreply.github.com>
2023-08-24 16:42:12 +0200

    Update section.blade.php

--- resources/views/components/section.blade.php
+++ resources/views/components/section.blade.php
@@ -1,4 +1,4 @@
-<div class="flex flex-col  font3 text-[#2f506c] text-lg mt-8 pt-8 bg-gray-50 min-h-[400px] max-h-[100%] px-5 adapt">
+<div class="flex flex-col  font3 text-[#2f506c] text-lg mt-8 pt-8 bg-gray-50 min-h-[400px] max-h-[100%] px-5 adapt lg:w-[100%]">
 
         <a class="flex flex-col  items-center pb-4 " href =  " {{ $url }}" >
             <h3 class="h3">{{ $label }}</h3>
