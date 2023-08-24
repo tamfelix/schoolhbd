d9ea704efe765def36813c31413dd493603ac6f7
tamfelix <84901549+tamfelix@users.noreply.github.com>
2023-08-24 13:48:27 +0200

    Add files via upload

---
+++ resources/lang/en/en/messages.php
@@ -0,0 +1,34 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'welcome' => "welcome to school's website!",
+    'saved' => 'the information is saved',
+    'updated' => 'the information is updated',
+    'failed' => 'operation could not be completed for a moment',
+    'enrolled' => "The student has been registered",
+    'sent' => "Email sent",
+    'enquiry' => "enquiry sent",
+    'submit' => 'submit',
+    'login' => 'Log-in',
+    'register' => "Register",
+    'search' => "Search",
+    'studname' => "Student's name",
+    'studsurname' => "Student's surname",
+    'subject' => 'subject',
+    'email' => 'email',
+    'enter' => 'enter your message',
+    'from' => 'from:',
+    'comments' => 'Comments:'
+];
---
+++ resources/lang/en/fr/sections.php
@@ -0,0 +1,66 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | SECTIONS' titles Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'calendar' => 'Calendrier',
+    'class' => 'Classe',
+    'paracha' => "Paracha de la semaine" ,
+    'services' => 'Services proposés',
+    'projects' => 'Projets',
+    'events' => 'Evenements',
+    'photos' => 'Gallerie de photos',
+    'about' => "A propos de l'école",
+    'program' => 'Programme',
+    'activities' => 'Activités',
+    'enquiries' => "Demandes d'inscriptions et visites",
+    'parents' => 'Parents',
+    'timetable' => 'Emploi du temps',
+    'lesson' => 'cours',
+    'see' => 'Voir emploi complet',
+    'announcements' => 'Annonces',
+    'read' => 'Lire plus',
+    'select' => "Selectionnez la classe pour visualiser le programme pour l'année scolaire:",
+    'weather'=> 'Météo',
+    'team' => 'Nos enseignants',
+    'reserve' => 'Reserver la visite',
+    'information' => 'Demande de renseignement',
+    'enroll' => "Fiche d'inscription en ligne",
+    'booklet' => 'Envoyer une brochure par poste',
+    'meals' => 'Répas',
+    'esupport' => 'Support de cours digital',
+    'extractivities' => 'Activites extra-scolaires',
+    'infoclass' => 'Information sur les cours',
+    'materials' => 'Materiel scolaire',
+    'finance' => 'Finance',
+    'question' => 'Questionnaire',
+    'contact' => 'Nous contacter',
+    'bank'  => 'Banque',
+    'banner1' => 'Venez pour les journées de portes ouvertes',
+    'banner2' => 'Decouvrez les resources en ligne Moodle',
+    'suppliers' => 'Fournisseurs',
+    'soon' => 'Cet element va etre rajouté prochenement.',
+    'parentmessage' =>'Quelques services utiles:',
+    'dashboard' => 'Panel administratif',
+    'comittee' => 'Comité',
+    'supplies' => 'Fournitures scolaires',
+    'graduation' => 'Maturité',
+    'state' => "Reglamentation d'état",
+    'reviews' => 'Feedback',
+    'equipment' => 'Equipement de classes',
+    'tostudents' => "Pour les étudiants",
+    'jobs' => 'Emplois'
+
+
+
+];
