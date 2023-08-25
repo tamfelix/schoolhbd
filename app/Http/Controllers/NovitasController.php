a18c9109b54ab9d1b3151be90e79dce99ae05500
tamfelix <84901549+tamfelix@users.noreply.github.com>
2023-08-25 14:36:15 +0200

    Update NovitasController.php

--- app/Http/Controllers/NovitasController.php
+++ app/Http/Controllers/NovitasController.php
@@ -69,7 +69,7 @@ class NovitasController extends Controller
      */
     public function index()
     {
-        $news = DB::table('novitas')->paginate(4);
+        $news = DB::table('novitas')->paginate(8);
 
         return view('layouts.default.news')->with([
             'news' => $news,
