751667afb09c61629af27477fc87eaeb8563c933
tamfelix <84901549+tamfelix@users.noreply.github.com>
2023-08-28 14:35:13 +0200

    Add files via upload

---
+++ resources/lang/en/sections.php
@@ -0,0 +1,70 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | SECTIONS' titles Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'calendar' => 'Calendar',
+    'class' => 'Class',
+    'paracha' => "Week's Torah portion" ,
+    'services' => 'Services proposed',
+    'projects' => 'Projects',
+    'events' => 'Events',
+    'photos' => 'Photo Gallery',
+    'about' => 'About school',
+    'program' => 'Academic program',
+    'activities' => 'Activities',
+    'enquiries' => 'Visits and enrollment',
+    'parents' => 'Parents',
+    'timetable' => 'Day Timetable',
+    'lesson' => 'lesson',
+    'see' => 'See full timetable',
+    'announcements' => 'Announcements',
+    'read'=> 'Read more',
+    'select' => 'Select the class to visualise the school program for that year',
+    'weather'=> 'Weather',
+    'team' => 'Our team',
+    'reserve' => 'Book a visit',
+    'information' => 'Ask for more information',
+    'enroll' => 'Enroll online',
+    'booklet' => 'Request to mail a booklet',
+    'meals' => 'Meals',
+    'esupport' => 'Online studying resources',
+    'extractivities' => 'Extra-school activities',
+    'infoclass' => 'Information on lessons',
+    'materials' => 'Study materials',
+    'finance' => 'Financials',
+    'question' => 'Survey',
+    'contact' => 'Contacts',
+    'bank'  => 'Bank account',
+    'banner1' => 'COME TO OPEN DAYS',
+    'banner2' => 'Discover our online resources on Moodle',
+    'suppliers' => 'Suppliers',
+    'soon' => 'This section will be added soon.',
+    'parentmessage' =>'Some usefull services',
+    'dashboard' => 'Dashboard',
+    'comittee' => 'Committee',
+    'supplies' => 'School supplies',
+    'graduation' => 'For graduates',
+    'state' => 'State regulation',
+    'reviews' => 'Reviews',
+    'equipment' => 'Classes equipment',
+    'tostudents' => 'For students',
+    'jobs' => 'Job openings',
+    'account'=>'Account n', 
+    'available' => 'Available courses are:',
+    'outline' => 'brief program year outline for this lesson:',
+    'detailsm' => 'More detailed summary and e-support will be available on Moodle platform',
+
+
+
+];
