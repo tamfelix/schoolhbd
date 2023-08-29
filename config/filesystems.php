0899c852e2aeaf6e2627160834423a497681b11b
tamfelix <84901549+tamfelix@users.noreply.github.com>
2023-08-29 16:52:35 +0200

    Update filesystems.php

--- config/filesystems.php
+++ config/filesystems.php
@@ -53,6 +53,13 @@ return [
             'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
         ],
 
+        'public_html' => [
+            'driver' => 'local',
+            'root' =>   base_path('../'),
+            'url' => env('APP_URL'),
+            'visibility' => 'public',
+        ],
+
     ],
 
     /*
