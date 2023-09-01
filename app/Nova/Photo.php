b0ba320bb4a7d9ac7a544be8fe0b9917f1d70cae
tamfelix <84901549+tamfelix@users.noreply.github.com>
2023-09-01 11:05:13 +0200

    Update Photo.php

--- app/Nova/Photo.php
+++ app/Nova/Photo.php
@@ -9,7 +9,7 @@ use Laravel\Nova\Fields\Text;
 use Laravel\Nova\Fields\Textarea;
 use Laravel\Nova\Fields\Date;
 use Laravel\Nova\Fields\Image;
-use Laravel\Nova\Fields\File;
+use Laravel\Nova\Fields\Avatar;
 use Laravel\Nova\Fields\Trix;
 
 class Photo extends Resource
@@ -47,11 +47,12 @@ class Photo extends Resource
     {
         return [
             ID::make()->sortable(),
+            Avatar::make('lien','link')->disk('public_html')->path('img/events'),
             Text::make('titre en fr','title_fr')->sortable(),
             Text::make('titre en ang','title_en')->sortable()->hideFromIndex(),
             Text::make('texte en fr','content_fr')->sortable()->hideFromIndex(),
             Text::make('texte en fr','content_fr')->sortable()->hideFromIndex(),
-            File::make('lien','link')->disk('public_html')->path('img/events'),
+            
             Date::make('date creation' , 'created_at')->sortable(),
         ];
     }
